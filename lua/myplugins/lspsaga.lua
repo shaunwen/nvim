b9fbfeaee4adf5f08e409b1efd5aa2a98fcdf4f7
Shaun Wen <shaunwen01@gmail.com>
2022-12-14 19:48:44 +1100

    Fix lsp_finder does not work for python and add keymap for more lspsaga actions

--- lua/myplugins/lspsaga.lua
+++ lua/myplugins/lspsaga.lua
@@ -3,7 +3,7 @@ if (not status) then return end
 
 saga.init_lsp_saga {
   server_filetype_map = {
-    typescript = 'typescript'
+    -- typescript = 'typescript'
   }
 }
 
@@ -14,3 +14,8 @@ vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
 vim.keymap.set('i', '<C-k>', '<Cmd>Lspsaga signature_help<CR>', opts)
 vim.keymap.set('n', 'gp', '<Cmd>Lspsaga peek_definition<CR>', opts)
 vim.keymap.set('n', 'gr', '<Cmd>Lspsaga rename<CR>', opts)
+vim.keymap.set({ 'n', 'v' }, '<leader>ca', '<cmd>Lspsaga code_action<CR>', opts)
+vim.keymap.set("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
+vim.keymap.set('n', ']e', '<cmd>Lspsaga diagnostic_jump_next<CR>', opts)
+vim.keymap.set('n', '<leader>ui', '<cmd>Lspsaga open_floaterm gitui<CR>', opts)
+vim.keymap.set('n', '<leader>o', '<cmd>Lspsaga outline<CR>', opts)
