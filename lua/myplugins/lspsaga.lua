92560a02616f6400663a75a4f63c3ed935e1a123
Shaun Wen <shaunwen01@gmail.com>
2023-01-15 20:45:47 +1100

    Update config for lspsaga breaking change

--- lua/myplugins/lspsaga.lua
+++ lua/myplugins/lspsaga.lua
@@ -1,11 +1,7 @@
 local status, saga = pcall(require, "lspsaga")
 if (not status) then return end
 
-saga.init_lsp_saga {
-  server_filetype_map = {
-    -- typescript = 'typescript'
-  }
-}
+saga.setup({})
 
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
@@ -18,3 +14,5 @@ vim.keymap.set("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
 vim.keymap.set('n', ']e', '<cmd>Lspsaga diagnostic_jump_next<CR>', opts)
 vim.keymap.set('n', '<leader>ui', '<cmd>Lspsaga open_floaterm gitui<CR>', opts)
 vim.keymap.set('n', '<leader>o', '<cmd>Lspsaga outline<CR>', opts)
+vim.keymap.set("n", "<Leader>ci", "<cmd>Lspsaga incoming_calls<CR>")
+vim.keymap.set("n", "<Leader>co", "<cmd>Lspsaga outgoing_calls<CR>")
