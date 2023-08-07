948898dafd52092975492f93b38be7940f8c4e7f
Shaun Wen <shaunwen01@gmail.com>
2023-08-07 21:39:23 +1000

    Update lspsaga config

--- lua/myplugins/lspsaga.lua
+++ lua/myplugins/lspsaga.lua
@@ -11,16 +11,27 @@ saga.setup({
     }
   },
   finder = {
+    default = 'ref+imp+def',
     keys = {
-      jump_to = 'p',
-      expand_or_jump = '<CR>',
+      quit = 'q',
+      toggle_or_open = '<CR>',
+      vsplit = 's',
+      split = 'i',
+      shuttle = '[w',
+      close = '<C-c>k'
     },
   },
+  outline = {
+    auto_preview = false,
+    keys = {
+      toggle_or_jump = '<CR>'
+    }
+  }
 })
 
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
-vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
+vim.keymap.set('n', 'gd', '<Cmd>Lspsaga finder<CR>', opts)
 vim.keymap.set('n', 'gp', '<Cmd>Lspsaga peek_definition<CR>', opts)
 vim.keymap.set('n', 'gr', '<Cmd>Lspsaga rename<CR>', opts)
 vim.keymap.set({ 'n', 'v' }, '<leader>ca', '<cmd>Lspsaga code_action<CR>', opts)
