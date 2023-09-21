c1765f2207756c2c4ed2ff15efb215f06c13753a
Shaun Wen <shaunwen01@gmail.com>
2023-09-21 21:02:37 +1000

    Remove redundant keymaps for rust-tools

--- lua/myplugins/rust-tools.lua
+++ lua/myplugins/rust-tools.lua
@@ -7,8 +7,6 @@ local function on_attach(client, bufnr)
   local opts = { noremap = true, silent = true }
   -- See `:help vim.diagnostic.*` for documentation on any of the below functions
   vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-  vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-  vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
   vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
   -- See `:help vim.lsp.*` for documentation on any of the below functions
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
