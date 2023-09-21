e12a1eac3db0895e0b8ec80f6711ea03086e5223
Shaun Wen <shaunwen01@gmail.com>
2023-09-21 21:04:26 +1000

    Improve keymap for hop motions

--- lua/myplugins/hop.lua
+++ lua/myplugins/hop.lua
@@ -1,8 +1,11 @@
 require 'hop'.setup { keys = 'etovxqpdygfblzhckisuran', term_seq_bias = 0.5 }
 
 -- Changing the default f keyword
-vim.api.nvim_set_keymap('', 'f', "<cmd>lua require'hop'.hint_char1()<cr>", {})
-vim.api.nvim_set_keymap('', '<Leader>s', "<cmd>lua require'hop'.hint_char2({ multi_windows = true })<cr>", {})
+vim.keymap.set({ "n", "v" }, 'f', "<cmd>lua require'hop'.hint_char2({ multi_windows = true })<cr>", {})
 
--- Pattern Matching with t keyword
+-- Pattern matching with t keyword
 vim.api.nvim_set_keymap('n', 't', "<cmd>HopPattern<CR>", { noremap = true })
+
+-- Line matching
+vim.keymap.set({ "n", "v" }, '<Leader><Leader>l', "<cmd>HopLine<CR>", { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>lm', "<cmd>HopLineMW<CR>", { noremap = true })
