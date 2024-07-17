69091d7ea4f3d18698ff7b2ff432a62ef0362727
Shaun Wen <shaunwen01@gmail.com>
2024-07-18 09:26:14 +1000

    Fix display of line number

--- lua/opts.lua
+++ lua/opts.lua
@@ -12,7 +12,7 @@ vim.opt.encoding = 'utf-8'
 vim.opt.termguicolors = true
 vim.o.guifont = "JetBrainsMono Nerd Font:h16" -- text below applies for VimScript
 vim.o.mouse = 'a'
-vim.wo.number = true
+vim.o.number = true
 vim.wo.signcolumn = 'yes'
 
 vim.opt.title = true
