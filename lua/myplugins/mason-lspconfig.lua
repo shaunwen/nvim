a3222ee158e4e78c964a93c12ed74f182b35735a
Shaun Wen <shaunwen01@gmail.com>
2025-07-29 23:14:05 +1000

    Upgrade plugins

--- lua/myplugins/mason-lspconfig.lua
+++ lua/myplugins/mason-lspconfig.lua
@@ -70,13 +70,12 @@ mason_lspconfig.setup {
   ensure_installed = vim.tbl_keys(servers),
 }
 
-mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-      filetypes = (servers[server_name] or {}).filetypes,
-    }
-  end
-}
+-- Manually set up each server for compatibility
+for server_name, config in pairs(servers) do
+  require('lspconfig')[server_name].setup {
+    capabilities = capabilities,
+    on_attach = on_attach,
+    settings = config,
+    filetypes = config.filetypes,
+  }
+end
