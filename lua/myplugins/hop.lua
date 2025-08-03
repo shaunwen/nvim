da1a9d796fc69b27b6e3a1149c079d4edb36da12
Shaun Wen <shaunwen01@gmail.com>
2025-08-03 15:24:46 +1000

    Update keymaps for hop to avoid confliction

--- lua/myplugins/hop.lua
+++ lua/myplugins/hop.lua
@@ -1,10 +1,10 @@
 require 'hop'.setup { keys = 'etovxqpdygfblzhckisuran', term_seq_bias = 0.5 }
 
 -- Changing the default f keyword
-vim.keymap.set({ "n", "v" }, 'f', "<cmd>lua require'hop'.hint_char2({ multi_windows = true })<cr>", {})
+vim.keymap.set({ "n", "v" }, 'ff', "<cmd>lua require'hop'.hint_char2({ multi_windows = true })<cr>", {})
 
 -- Pattern matching with t keyword
-vim.api.nvim_set_keymap('n', 't', "<cmd>HopPattern<CR>", { noremap = true })
+vim.api.nvim_set_keymap('n', ',t', "<cmd>HopPattern<CR>", { noremap = true })
 
 -- Line matching
 vim.keymap.set({ "n", "v" }, '<Leader><Leader>l', "<cmd>HopLine<CR>", { noremap = true })
