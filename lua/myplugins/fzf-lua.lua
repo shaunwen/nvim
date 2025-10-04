cef30c21a6c4dda524b9390ec1d92bc0114323e8
Shaun Wen <shaunwen01@gmail.com>
2025-10-05 10:04:58 +1100

    Configure image browsing for fzf-lua

--- lua/myplugins/fzf-lua.lua
+++ lua/myplugins/fzf-lua.lua
@@ -4,4 +4,17 @@ require('fzf-lua').setup {
       ['ctrl-q'] = 'select-all+accept',
     },
   },
+  previewers = {
+    builtin = {
+      -- use chafa for real image files
+      extensions = {
+        ['png'] = { 'chafa', '--size=80x40', '--format=symbols', '{file}' },
+        ['jpg'] = { 'chafa', '--size=80x40', '--format=symbols', '{file}' },
+        ['jpeg'] = { 'chafa', '--size=80x40', '--format=symbols', '{file}' },
+        ['gif'] = { 'chafa', '--size=80x40', '--animate=false', '{file}' },
+        ['webp'] = { 'chafa', '--size=80x40', '--format=symbols', '{file}' },
+        ['svg'] = { 'chafa', '--size=80x40', '{file}' },
+      },
+    },
+  },
 }
